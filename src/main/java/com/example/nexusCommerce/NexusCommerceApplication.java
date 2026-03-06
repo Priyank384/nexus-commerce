92249f3a178cf6f9100a231aa1682d6c44d4ca5d
Priyank384 <priyankgupta384@gmail.com>
2026-03-07 03:52:02 +0530

    Changed the Application.java file

--- src/main/java/com/example/nexusCommerce/NexusCommerceApplication.java
+++ src/main/java/com/example/nexusCommerce/NexusCommerceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FakeCommerceApplication {
+public class NexusCommerceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(FakeCommerceApplication.class, args);
+		SpringApplication.run(NexusCommerceApplication.class, args);
 	}
 
 }
